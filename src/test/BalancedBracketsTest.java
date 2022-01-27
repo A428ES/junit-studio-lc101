12f50b6e937b373dec623498b66ba24036b607dc
Sergio Estrada <estrada.serg@gmail.com>
2022-01-27 01:45:54 -0600

    bonus mission done

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,8 +1,8 @@
 package test;
 
 import org.junit.Test;
-
 import static org.junit.Assert.*;
+import main.BalancedBrackets;
 
 public class BalancedBracketsTest {
 
@@ -12,5 +12,55 @@ public class BalancedBracketsTest {
         assertEquals(true, true);
     }
 
+    @Test
+    public void emptyString(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
 
+    @Test public void noBrackets(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("LaunchCode"));
+    }
+
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test public void LaunchCodeSingleBracket(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[LaunchCode]"));
+    }
+
+    @Test public void CodeInsideBracket(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch[Code]"));
+    }
+
+    @Test public void LaunchCodeRightSide(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]LaunchCode"));
+    }
+
+    @Test public void slackTestOne(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[ [ ] ]"));
+    }
+
+
+    @Test
+    public void LaunchCodeLeftOpen(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[LaunchCode"));
+    }
+
+    @Test public void LaunchCodeRightOpen(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets(("Launch]Code")));
+    }
+
+    @Test public void LeftOpenSingle(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    @Test public void ImproperNest(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    @Test public void slackTestTwo(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[ ] ] ["));
+    }
 }
